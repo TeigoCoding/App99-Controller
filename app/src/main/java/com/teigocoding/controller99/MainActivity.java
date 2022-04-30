6de8bcbe5739297cc6b8d832cb75de701c970b15
Tiago Frota Vallim <tiago.vallim1992@gmail.com>
2022-04-29 22:42:44 -0300

    Menus básicos completos.

--- app/src/main/java/com/teigocoding/controller99/MainActivity.java
+++ app/src/main/java/com/teigocoding/controller99/MainActivity.java
@@ -1,14 +1,107 @@
 package com.teigocoding.controller99;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlaylot);
+
+    }
+    public void ClickMenu (View view){
+        openDrawer (drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    }
+
+    public void ClickLogo (View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+    }
+
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+
+        Intent intent = new Intent(activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+
+    }
+    public void ClickRuns (View view){
+
+        redirectActivity(this, MenuRunsActivity.class);
+
+    }
+    public void ClickEarns (View view){
+
+        redirectActivity(this, MenuEarnsActivity.class);
+
+    }
+    public void ClickAboutus (View view){
+
+        redirectActivity(this, MenuAboutUsActivity.class);
+
+    }
+    public void ClickLogout (View view){
+
+        logout(this);
+
+    }
+
+
+    public static void logout(final Activity activity) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        builder.setTitle("Sair");
+        builder.setMessage("Você realmente deseja sair?");
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        closeDrawer(drawerLayout);
     }
 }
---
+++ app/src/main/java/com/teigocoding/controller99/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.teigocoding.controller99;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        finish();
+    }
+}
